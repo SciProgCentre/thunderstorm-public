6b5c9740e91bbb4820766b38204d9cef6ad43c56
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2019-10-08 18:38:07 +0300

    Add unit

--- VHF/main.py
+++ VHF/main.py
@@ -9,8 +9,8 @@ from vhf.process import process_all_files
 def main():
     time_step = 1  # ns
     path = os.path.join(".", "data")
-    path = "/home/zelenyy/storage/data/sri-thunderstorm/VHF/first/"
-    observed_point = np.array([1e3,0,0])
+    # path = "/home/zelenyy/storage/data/sri-thunderstorm/VHF/first/"
+    observed_point = np.array([1e3,0,0]) # meter
     signal_electric, signal_magnetic, time = process_all_files(path, observed_point, time_step)
     print(save_result("result.hdf5", signal_electric, signal_magnetic, time))
 
